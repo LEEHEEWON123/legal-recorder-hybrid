77fe9720641d90455e80d73fca7b3c5cdd234530
LEEHEEWON123 <heh139811@dobedub.com>
2026-05-26 11:47:00 +0900

    feat: app theme, bottom nav, placeholder screens

---
+++ lib/screens/recording_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecordingListScreen extends StatelessWidget {
+  const RecordingListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('목록 화면')));
+  }
+}
